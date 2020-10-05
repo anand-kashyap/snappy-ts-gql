6c8546878d72e68cf639870ad61cbf8fb61b9794
Anand Kashyap <anandkashyap60@gmail.com>
2020-10-05 16:14:21 +0530

    updated build

--- build.js
+++ build.js
@@ -1,16 +1,20 @@
 const childProcess = require('child_process');
+const fs = require('fs');
 
 try {
     // Transpile the typescript files
-    const proc = childProcess.exec('tsc --build tsconfig.prod.json');
+    const proc = childProcess.exec('tsc --build tsconfig.prod.json && cp ./src/schema.gql ./functions/bundle');
     proc.on('error', (err) => {
         console.log('err', err)
     })
     proc.on('close', (code) => {
+        const fils = fs.readdirSync('./functions/bundle');
+        console.log('finished', fils);
         if (code !== 0) {
             throw Error("Build failed")
         }
-    })
+    });
+
 } catch (err) {
     console.log(err);
 }
