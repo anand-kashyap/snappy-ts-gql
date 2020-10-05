c07992e6d8559a1cc87ae8dfdb78e929f67aa34b
Anand Kashyap <anandkashyap60@gmail.com>
2020-10-05 15:36:27 +0530

    update build config

---
+++ build.js
@@ -0,0 +1,16 @@
+const childProcess = require('child_process');
+
+try {
+    // Transpile the typescript files
+    const proc = childProcess.exec('tsc --build tsconfig.prod.json');
+    proc.on('error', (err) => {
+        console.log('err', err)
+    })
+    proc.on('close', (code) => {
+        if (code !== 0) {
+            throw Error("Build failed")
+        }
+    })
+} catch (err) {
+    console.log(err);
+}
